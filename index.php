f8506ec67fb0d2ecedd244811c6e9bfbe3adf39f
Jeffrey Cho <jeffcho59@gmail.com>
2014-09-11 17:42:42 -0400

    test commit

--- index.php
+++ index.php
@@ -1,7 +1,18 @@
 <?php
-  //This is how you comment one line
-  echo 'Hello World';
-  /*
-    This is a block comment.
-  */
+
+  $obj = new example;
+  $obj->name = 'steve';
+  print_r($obj);
+  $obj2 = new example('jeff');
+  
+  class example{
+    public $name;
+      function__construct(){
+        $this->name = $name;
+        echo 'Hello World...' . $this->name;
+	}
+	function printHellowWorld(){
+	echo 'Hello World...' . $this->name;
+	}
+    }
 ?>
